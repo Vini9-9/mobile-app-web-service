38e5fdaeef2d78a83f08a6f1b570100c00e6873d
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-03-22 14:58:18 -0300

    Leitura query string param opcionais

--- mobile-app-ws/src/main/java/com/vipdsilva/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/vipdsilva/app/ws/ui/controller/UserController.java
@@ -6,12 +6,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	@GetMapping
+	public String getUser(
+			@RequestParam(value="page", defaultValue="1") int page,
+			@RequestParam(value="limit", defaultValue="10") int limit,
+			@RequestParam(value="sort", defaultValue="asc") String sort
+			) 
+	{
+		return "get users was callled with page = " + page 
+				+ " and limit = " + limit
+				+ " and sort = " + sort;
+	}
+	
 	@GetMapping(path = "/{userId}")
 	public String getUser(@PathVariable String userId) {
 		return "get users was callled with userId = " + userId;
